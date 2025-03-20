cc3c637743b1321e3a8d9b98b8998a63f7a484a1
Abdulrahman-haytham <140040845+Abdulrahman-haytham@users.noreply.github.com>
2025-03-21 02:06:35 +0400

    Fix case issue in filename

--- models/user.js
+++ models/user.js
@@ -99,7 +99,6 @@ userSchema.methods.comparePassword = async function (password) {
 };
 
 
-const User = mongoose.model('User', userSchema);
-   module.exports = User;
 
+module.exports = mongoose.model('User', userSchema);
    
